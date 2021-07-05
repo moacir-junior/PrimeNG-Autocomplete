f123d62d276410c9540425289c52c596fb799876
Moacir Zimermann <moacir.zimermann@senior.com.br>
2021-07-05 16:13:09 -0300

    autocomplete issue example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { Country } from './country';
+import { CountryService } from "./countryservice";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'autocomplete-issue';
+  @ViewChild("autocompleteField") autocompleteField: any;
+
+  selectedCountry!: Country;
+  countries!: Country[];
+  filteredCountries!: Country[];
+  selectedCountries!: Country[];
+  selectedCountryAdvanced!: Country[];
+
+  constructor(
+    private countryService: CountryService,
+  ) {}
+
+  ngOnInit() {
+    this.countryService.getCountries().then(countries => {
+      this.countries = countries;
+    });
+  }
+
+  filterCountry(event: any) {
+    //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
+    let filtered: any[] = [];
+    let query = event.query;
+    for (let i = 0; i < this.countries.length; i++) {
+      let country = this.countries[i];
+      if (country.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(country);
+      }
+    }
+
+    this.filteredCountries = filtered;
+  }
+
+  onSelect(event: any) {
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {AutoCompleteModule} from 'primeng/autocomplete';
 
 import { AppComponent } from './app.component';
+import { CountryService } from './countryservice';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AutoCompleteModule
   ],
-  providers: [],
+  providers: [
+    CountryService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
